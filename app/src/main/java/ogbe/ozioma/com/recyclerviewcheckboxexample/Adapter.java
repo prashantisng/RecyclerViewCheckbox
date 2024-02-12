94e9339fa959c90cdd74e53d4983a9a1e8568fe7
android <outthinkingandroid@gmail.com>
2024-02-12 12:03:33 +0530

    Recyclerview with checkbox

--- app/src/main/java/ogbe/ozioma/com/recyclerviewcheckboxexample/Adapter.java
+++ app/src/main/java/ogbe/ozioma/com/recyclerviewcheckboxexample/Adapter.java
@@ -1,12 +1,13 @@
 package ogbe.ozioma.com.recyclerviewcheckboxexample;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckedTextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
